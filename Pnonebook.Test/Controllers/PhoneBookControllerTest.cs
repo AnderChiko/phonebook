4d4f078ba22d29b4854b9b532bbca129602ea2b1
anderson <anderson@healthsystems.co.za>
2021-02-11 10:56:09 +0200

    added test project

---
+++ Pnonebook.Test/Controllers/PhoneBookControllerTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pnonebook.Test.Controllers
+{
+    public class PhoneBookControllerTest
+    {
+    }
+}
---
+++ Pnonebook.Test/TestHelpers/TestData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pnonebook.Test.TestHelpers
+{
+    public static class TestData
+    {
+        
+    }
+}
